118b0255ceaffdfa2a2777843c14c93e74af4f1d
EmilianaManzo <emiliana.ma96@gmail.com>
2024-02-23 16:27:58 +0100

    [add] if

--- javascript/script.js
+++ javascript/script.js
@@ -4,11 +4,18 @@ console.log(listmail);
 
 let mailUtente = prompt('Inserisci la tua email');
 
+let isValidData = false
+
+
 for (let i=0; i < listmail.length; i++){
   console.log(i);
-
   if (mailUtente == listmail[i]){
-    
-
+    isValidData = true
   }
+}
+
+if (isValidData){
+  alert('Email inserita non presente nel sistema. Riprovare');
+}else{
+  document.getElementById('mailbox').innerHTML = 'La mail inserita è corretta. Adesso puoi giocare ai dadi.'
 }
