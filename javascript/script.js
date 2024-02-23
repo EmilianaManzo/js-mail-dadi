597fae8edad5c04f8172c90a7f27d91a01533841
EmilianaManzo <emiliana.ma96@gmail.com>
2024-02-23 17:26:21 +0100

    [add] dadi

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+// MAIL 
+
 const listmail = ['emy@libero.it', 'giulio@gmail.com', 'emy3@gmail.com', 'boolean@gmail.com', 'machimelohafattofare@libero.it','ciaoboolean@gmail.com'];
 
 console.log(listmail);
@@ -15,7 +17,28 @@ for (let i=0; i < listmail.length; i++){
 }
 
 if (isValidData){
-  alert('Email inserita non presente nel sistema. Riprovare');
-}else{
   document.getElementById('mailbox').innerHTML = 'La mail inserita è corretta. Adesso puoi giocare ai dadi.'
+}else{
+  alert('Email inserita non presente nel sistema. Riprovare');
+  document.getElementById('mailbox').innerHTML = 'La mail inserita non è corretta.'
+}
+
+
+// DADI 
+
+let dicepc = (Math.ceil(Math.random()*6));
+let diceuser = (Math.ceil(Math.random()*6));
+
+console.log(dicepc, diceuser);
+
+document.getElementById('diceuser').append(diceuser);
+document.getElementById('dicepc').append(dicepc);
+
+
+if (dicepc > diceuser){
+  document.getElementById('resulte').innerHTML = 'Ha vinto il PC!'
+}else if (diceuser > dicepc){
+  document.getElementById('resulte').innerHTML = 'Hai vinto!'
+}else if (dicepc == diceuser){
+  document.getElementById('resulte').innerHTML = 'Il risultato è pari'
 }
